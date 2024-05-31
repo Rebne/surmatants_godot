9af665156b6cdf199d01ecf9c0324c8fe89218d1
student <kooduser@pupil.kood.tech>
2024-05-31 22:30:05 +0300

    Added player sprite

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -11,8 +11,8 @@ func _on_play_pressed():
 	get_tree().change_scene_to_file("res://Scenes/game.tscn")
 
 func _on_options_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://Scenes/options.tscn")
 
 
 func _on_exit_pressed():
-	pass # Replace with function body.
+	get_tree().quit()
