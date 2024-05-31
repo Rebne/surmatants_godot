0a5ae56f2b9c6fc958eb3b5df5dd2cdc8802b160
student <kooduser@pupil.kood.tech>
2024-05-31 16:16:16 +0300

    Added other files besides README too

---
+++ Scripts/main_menu.gd
@@ -0,0 +1,17 @@
+extends Control
+@onready var background = $Background
+
+const FRAME_DURATION = 0.1
+
+func _ready():
+	for i in range(6):
+		background.texture.set_frame_duration(i, FRAME_DURATION)
+		
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://Scenes/game.tscn")
+
+func _on_options_pressed():
+	get_tree().change_scene_to_file("res://Scenes/options.tscn")
+
+func _on_quit_pressed():
+	get_tree().quit()
